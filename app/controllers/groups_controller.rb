dcb37d138f339f0d6efc4c18c67dfc07341c8e7c
周泽宇 <zhouzeyuinchina@gmail.com>
2018-03-21 17:07:38 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -42,7 +42,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(group).permit(:title, :description)
+    params.require(:group).permit(:title, :description)
   end
 
 end
