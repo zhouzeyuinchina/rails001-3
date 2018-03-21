55f5c9e1919f3b1ce22213df2508462d5bb3ac4b
周泽宇 <zhouzeyuinchina@gmail.com>
2018-03-21 17:02:25 +0800

    create groups index

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,48 @@
+class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path warning: "Delete Success!"
+  end
+
+  private
+  def group_params
+    params.require(group).permit(:title, :description)
+  end
+
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:warning] = "Darling"
+  end
 end
